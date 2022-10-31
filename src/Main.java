830da4a4e4014e99483f36eea62721f298bd127a
rocean <rocean74@gmail.com>
2022-11-01 00:20:58 +0200

    count Words

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        ReadApi process = new ReadApi();
+        ReadApi process = new ReadApi("1942", 10);
 
         process.start();
 
--- src/ReadApi.java
+++ src/ReadApi.java
@@ -2,13 +2,32 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.MatchResult;
+import java.util.regex.Pattern;
 
 public class ReadApi extends Thread {
-    private final String API_URL = "http://numbersapi.com/1942/year";
+    private String year;
+    private String API_URL;
+    private int k;
+    private Map<String, Integer> commonWords = new HashMap<String, Integer>();
+    private Map<String, Integer> wordsInYear = new HashMap<String, Integer>();
+
+    public ReadApi(String year, int k) {
+        this.year = year;
+        this.k = k;
+        API_URL = "http://numbersapi.com/" + year + "/year";
+    }
 
     @Override
     public void run() {
-        System.out.printf(loadDataFromUrl());
+        for(int i=0;i<k;i++) {
+            countWords(loadDataFromUrl());
+        }
+
+//        System.out.println(commonWords);
+//        System.out.println(wordsInYear.get(year));
     }
 
     /**
@@ -35,5 +54,41 @@ public class ReadApi extends Thread {
         }
 
         return result.toString();
+    }
+
+    private String[] getWordsInText(String text) {
+        return Pattern.compile("\\b(?:\\w|-)+\\b")
+                .matcher(text)
+                .results()
+                .map(MatchResult::group)
+                .toArray(String[]::new);
+    }
+
+    private void addWordsCounterInYear(int counter) {
+        if(wordsInYear.containsKey(year)) {
+            int sum = wordsInYear.get(year) + counter;
+            wordsInYear.put(year, sum);
+        } else {
+            wordsInYear.put(year, counter);
+        }
+    }
+
+    private void countWords(String text) {
+        String[] words = getWordsInText(text);
+
+        for(String word : words) {
+            word = word.toLowerCase();
+
+            if(word.length()<4) {
+                if(commonWords.containsKey(word)) {
+                    int sum = commonWords.get(word) + 1;
+                    commonWords.put(word, sum);
+                } else {
+                    commonWords.put(word, 1);
+                }
+            }
+        }
+
+        addWordsCounterInYear(words.length);
     }
 }
